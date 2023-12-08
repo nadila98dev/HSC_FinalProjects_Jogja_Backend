47ca39c58354d5857360d1e1cb142b49bb66d1e6
NihhZaeyy <nizaralmas11@gmail.com>
2023-12-08 20:31:36 +0700

    add:account

--- src/app/account/controller.js
+++ src/app/account/controller.js
@@ -1,31 +1,89 @@
+const { StatusCodes } = require("http-status-codes");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const { StatusCodes } = require("http-status-codes");
 
+// Update user's address
+const updateAddress = async (req, res, next) => {
+  try {
+    const { address } = req.body;
+    const userId = req.user.id;
 
-module.exports = {
-    update: async(req, res) => {
-        try {
+    const updatedUser = await prisma.user.update({
+      where: { id: userId },
+      data: { address },
+    });
 
-            const {name, address, phone } = req.body
+    res
+      .status(StatusCodes.OK)
+      .json({ message: "Address updated successfully", user: updatedUser });
+  } catch (error) {
+    next(error);
+  }
+};
 
-            
-            
-        } catch (err) {
-            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-                success: false,
-                message: err.message
-            })
-        }
-    },
-    updateImage: async(req, res) => {
-        try {
-            
-        } catch (err) {
-            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-                success: false,
-                message: err.message
-            })
-        }
+// Update user's phone number
+const updatePhoneNumber = async (req, res, next) => {
+  try {
+    const { phone } = req.body;
+    const userId = req.user.id;
+
+    const updatedUser = await prisma.user.update({
+      where: { id: userId },
+      data: { phone },
+    });
+
+    res.status(StatusCodes.OK).json({
+      message: "Phone number updated successfully",
+      user: updatedUser,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Upload avatar image
+const uploadAvatar = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+
+    if (!req.file) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ error: true, message: "Please provide an image" });
     }
-}
+
+    const updatedUser = await prisma.user.update({
+      where: { id: userId },
+      data: { avatar: req.file.path },
+    });
+
+    res
+      .status(StatusCodes.OK)
+      .json({ message: "Avatar updated successfully", user: updatedUser });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Get account data
+const getAccount = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    if (!user) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ error: true, message: "User not found" });
+    }
+
+    res.status(StatusCodes.OK).json({ user });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { updateAddress, updatePhoneNumber, uploadAvatar, getAccount };
--- src/index.js
+++ src/index.js
@@ -40,7 +40,6 @@ app.use(`${version}/admin`, userRouter);
 app.use(`${version}/account`, accountRouter);
 
 // category
-
 app.use(`${version}/categories`, categoryRouter);
 
 // items
