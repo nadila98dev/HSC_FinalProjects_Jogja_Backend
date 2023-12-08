2e00f4c448e7393ed0f0de051e766799274891e6
komporbakar <laravelgg00@gmail.com>
2023-12-08 15:02:18 +0700

    fixing function midtrans

--- src/utils/midtrans.js
+++ src/utils/midtrans.js
@@ -49,12 +49,6 @@ const PaymentGateway = async (price, trxid, itemDetails, user) => {
 
   const res = await snap.createTransactionToken(parameter)
   console.log(res)
-  // .then((transaction) => {
-  //   // transaction redirect_url
-  //   let redirectUrl = transaction.redirect_url;
-  //   console.log("redirectUrl:", redirectUrl);
-  //   return redirectUrl;
-  // });
   return { res, parameter };
 };
 
